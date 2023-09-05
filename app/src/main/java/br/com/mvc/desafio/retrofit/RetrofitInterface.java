048a5cb45f0888a96ae91cbfb47aa529087faaf8
David Lim Bok Siok <davidlimboksiok@gmail.com>
2023-09-05 15:36:52 -0300

    adjust

--- app/src/main/java/br/com/mvc/desafio/retrofit/RetrofitInterface.java
+++ app/src/main/java/br/com/mvc/desafio/retrofit/RetrofitInterface.java
@@ -4,23 +4,17 @@ import java.util.List;
 
 import br.com.mvc.desafio.model.Post;
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 
 public interface RetrofitInterface {
 
     @GET("posts")
     Call<List<Post>> getPosts();
-//
-//    Retrofit retrofit = new Retrofit.Builder()
-//            .baseUrl(BASE_URL)
-//            .addConverterFactory(GsonConverterFactory.create())
-//            .build();
-//
-//    RetrofitInterface retrofitInterface = retrofit.create(RetrofitInterface.class);
-//
-//        static RetrofitInterface getInstance() {
-//            return retrofitInterface;
-//        }
+
+    @POST
+    Call<Post> createPost(@Body Post post);
 
 }
 
--- app/src/main/java/br/com/mvc/desafio/view/MainActivity.java
+++ app/src/main/java/br/com/mvc/desafio/view/MainActivity.java
@@ -2,15 +2,11 @@ package br.com.mvc.desafio.view;
 
 import static br.com.mvc.desafio.BuildConfig.BASE_URL;
 
-import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteStatement;
 import android.os.Bundle;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
-import java.util.ArrayList;
+
 import java.util.List;
 
 import br.com.mvc.desafio.R;
@@ -26,20 +22,12 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
 
-    public ListView listViewData;
-
-    private SQLiteDatabase database;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        createDatabase();
-
-
-//        textViewResult = findViewById(R.id.text_view_result);
-        listViewData = findViewById(R.id.listViewData);
+        textViewResult = findViewById(R.id.text_view_result);
 
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(BASE_URL)
@@ -56,14 +44,15 @@ public class MainActivity extends AppCompatActivity {
                 if (!response.isSuccessful()) {
                     textViewResult.setText("Code:" + response.code());
                 }
-                List<Post> posts = response.body();
 
+                List<Post> posts = response.body();
                 for (Post post : posts) {
-                    String id = String.valueOf(post.getId());
-                    String userId = String.valueOf(post.getUserId());
-                    String title = post.getTitle() ;
-                    String text = post.getText() ;
-                    insertDataFromApi(id, userId, title,text);
+                    String content = "";
+                     content += "Id: " + post.getId() + "\n";
+                    content += "User ID: " + post.getUserId()+ "\n";
+                    content += "Title: " + post.getTitle() + "\n";
+                    content += "Text: " + post.getText() + "\n\n";
+                    textViewResult.append(content);
 
                 }
             }
@@ -73,62 +62,5 @@ public class MainActivity extends AppCompatActivity {
                 textViewResult.setText(t.getMessage());
             }
         });
-        listData();
-    }
-
-
-
-    private void createDatabase() {
-        try {
-            database = openOrCreateDatabase("crudapp", MODE_PRIVATE, null);
-            database.execSQL("CREATE TABLE IF NOT EXISTS " +
-                    "Post(" +
-                    " userId INTEGER PRIMARY KEY AUTOINCREMENT" +
-                    ",id INTEGER" +
-                    ",title VARCHAR"+
-                    ",text VARCHAR)");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void listData() {
-        try {
-            database = openOrCreateDatabase("crudapp", MODE_PRIVATE, null);
-            Cursor cursor = database.rawQuery("SELECT userId, id, title, text from Post ", null);
-            ArrayList<String> lines = new ArrayList<>();
-            ArrayAdapter myAdapter = new ArrayAdapter<>(
-                    this, android.R.layout.activity_list_item, android.R.id.text1, lines
-            );
-            listViewData.setAdapter(myAdapter);
-            cursor.moveToFirst();
-            while (cursor != null){
-                lines.add(cursor.getString(1));
-                cursor.moveToNext();
-            }
-
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    private void insertDataFromApi(String userId, String id, String title, String text) {
-
-        try {
-            //Init database
-            database = openOrCreateDatabase("crudapp", MODE_PRIVATE, null);
-            String sql = "INSERT INTO Post (userId, id, title, text)" + "VALUES (?, ?, ?, ?)";
-            SQLiteStatement stmt = database.compileStatement(sql);
-
-            stmt.bindString(1, userId);
-            stmt.bindString(2, id);
-            stmt.bindString(3, title);
-            stmt.bindString(4, text);
-            stmt.executeInsert();
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
